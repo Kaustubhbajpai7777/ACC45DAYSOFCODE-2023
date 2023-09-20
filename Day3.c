6c90b948e3fb45b6b78f0340823d46b021988d0a
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2023-09-20 22:54:11 +0530

    Create Day3.c
    Area of rectangle

---
+++ Day3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int Length,Breadth,Area;
+    printf("Enter value of Length\n");
+    scanf("%d",&Length);
+    printf("Enter value of Breadth\n");
+    scanf("%d",&Breadth);
+    printf("The area of rectangle is %d\n",Area=Length*Breadth);
+    return 0;
+}
