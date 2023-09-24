0e280b1e0c3c98ea8b442a6f69a5db8f0a66e838
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2023-09-24 23:15:57 +0530

    Create Day7.c
    Area of Circle and Volume of Cylinder

---
+++ Day7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius,height;
+    float Area,Volume,pi = 3.14;
+    printf("give the radius, height of cylinder\n");
+    scanf("%d %d", &radius, &height);
+    printf("Area of Circle is %f\n",Area=pi*radius*radius);
+    printf("Volume of cylinder is %f\n",Volume=pi*radius*radius*height);
+    return 0;
+}
