30aa0f47f1b58404638f50bf621f8482e7e155de
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2023-09-29 21:56:35 +0530

    Create Day12.c
    To check the divisibility

---
+++ Day12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // Write a programe to check the divisibility by 97
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    printf("The Divisibility test return %d\n",num%97);
+    return 0;
+}
