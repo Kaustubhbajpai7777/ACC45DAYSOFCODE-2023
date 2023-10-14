019a5a70617ba4ae898f598487aec713a9b3edf6
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2023-10-14 22:35:36 +0530

    Create Day27.c

---
+++ Day27.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter an int value\n");
+    scanf("%d",&a);
+    printf("Octal representation is %o\n",a);
+    printf("Hex representation is %x\n",a);
+
+    return 0;
+}
