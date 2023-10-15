eacab97ca12565a3f39f3cbd6ccb32448eca1971
kaustubh Bajpai <144876616+Kaustubhbajpai7777@users.noreply.github.com>
2023-10-15 22:08:57 +0530

    Create Day28.c

---
+++ Day28.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int age = 18;
+  if (age > 18) {
+    cout << "Eligible to Vote";
+  } else {
+    cout << "Not Eligible";
+  }
+  return 0;
+}
